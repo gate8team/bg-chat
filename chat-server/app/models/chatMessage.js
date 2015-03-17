5719d880b9ee0b9be3c522d0435e90ff85e67ae7
ashabanov <me.antony.rain@gmail.com>
2015-03-17 21:12:43 +0300

    Added chat message token on message creation.

--- chat-server/app/models/chatMessage.js
+++ chat-server/app/models/chatMessage.js
@@ -1,4 +1,5 @@
-Waterline = require('waterline')
+var Waterline = require('waterline'),
+    Crypto = require('crypto');
 
 var ChatMessage = Waterline.Collection.extend({
   identity: 'chat_message',
@@ -15,6 +16,12 @@ var ChatMessage = Waterline.Collection.extend({
     token: {
       type: 'string',
       maxLength: 65535
+    },
+    // before create
+    beforeCreate: function(values, cb) {
+      Crypto.randomBytes(48, function(ex, buf) {
+        values.token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+      });
     }
   }
 });
