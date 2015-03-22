a3643b818b348c43d6b278cea25531e5e53c86a0
ashabanov <me.antony.rain@gmail.com>
2015-03-22 16:58:55 +0300

    Cleared the server app.

--- chat-server/app/models/index.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-// simple loader of whole models in the current folder
-require('fs').readdirSync(__dirname).forEach(function (file) {
-  if (file === 'index.js') return;
-  module.exports[path.basename(file, '.js')] = require(path.join(__dirname, file));
-});
