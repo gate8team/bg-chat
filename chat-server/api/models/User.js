391aa1885b517be0bac0a83741cbf1d6d1a99059
ashabanov <me.antony.rain@gmail.com>
2015-03-29 13:23:00 +0300

    Added lifecycle callbacks for User Model.

--- chat-server/api/models/User.js
+++ chat-server/api/models/User.js
@@ -5,12 +5,30 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var bcrypt = require('bcrypt');
+
 module.exports = {
 
   attributes: {
-    name : { type: 'string' },
-    email : { type: 'string' },
-    password : { type: 'string' }
+
+    name : {
+      type: 'string',
+      required: true
+    },
+
+    email : {type: 'string' },
+
+    password : {
+      type: 'string',
+      required: true
+    }
+  },
+  beforeCreate: function(values, cb) {
+    bcrypt.hash(values.password, 10, function(err, hash) {
+      if(err) return cb(err);
+      values.password = hash;
+      cb();
+    });
   }
 };
 
